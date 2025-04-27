c8050c3f48db8388295b446e3a5cf5fc8230a92e
NAVANEEDGOD <navavworld@gmail.com>
2025-04-27 12:19:58 +0700

    Initial commit

---
+++ seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO Admins (username, password, email) VALUES
+('admin1', 'hashed_pass1', 'admin1@example.com'),
+('admin2', 'hashed_pass2', 'admin2@example.com'),
+('admin3', 'hashed_pass3', 'admin3@example.com');
